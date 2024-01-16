6f094e8fd6190bc19ccb8e4a416ac5016d5f22a7
Rob Smith <smith.rob@student.greenriver.edu>
2024-01-16 12:53:26 -0500

    F3 Setup

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
+/*Rob Smith
+ *Jan 2024
+ *https://rsmiths.greenriverdev.com/328/diner/
+ *Site Controller
+*/
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Instantiate the f3 base class
+$f3 = Base::instance();
+
+$f3->run();
+
 echo "Diner";
 echo "Now";
