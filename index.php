2f1fda8ccc889902fd2b3d9401d9e9b9eaab3764
Rob Smith <smith.rob@student.greenriver.edu>
2024-01-23 13:09:52 -0500

    Adding routes and order forms

--- index.php
+++ index.php
@@ -14,10 +14,13 @@ require_once ('vendor/autoload.php');
 // Instantiate the f3 base class
 $f3 = Base::instance();
 
-$f3->route('GET /', function () {
-    $view = new Template();
-    echo $view->render('views/home.html');
+$f3->route('GET /order1', function () {
+    echo "orderformpart1";
+//    $view = new Template();
+//    echo $view->render('views/order1.html');
 });
+
+#f3->route('GET/',
 
 $f3->run();
 
