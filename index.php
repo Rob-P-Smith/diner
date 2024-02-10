6421e4d51ec4db8578176aa8f77b115bf4329894
Rob Smith <smith.rob@student.greenriver.edu>
2024-02-10 14:48:23 -0500

    Changes from last class

--- index.php
+++ index.php
@@ -10,15 +10,15 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Require the autoload file
-require_once('vendor/autoload.php');
-require_once('model/data-layer.php');
-require_once('model/validator.php');
+require_once ('vendor/autoload.php');
+require_once ('model/data-layer.php');
+require_once ('model/validate.php');
 
 // Instantiate Fat-Free framework (F3)
 $f3 = Base::instance(); //static method
 
 // Define a default route
-$f3->route('GET /', function () {
+$f3->route('GET /', function() {
     //echo "My Diner";
 
     // Display a view page
@@ -27,7 +27,7 @@ $f3->route('GET /', function () {
 });
 
 // Define a breakfast route
-$f3->route('GET /breakfast', function () {
+$f3->route('GET /breakfast', function() {
     //echo "Breakfast";
 
     // Display a view page
@@ -36,39 +36,45 @@ $f3->route('GET /breakfast', function () {
 });
 
 // Define a order form 1 route
-$f3->route('GET|POST /order1', function ($f3) {
+$f3->route('GET|POST /order1', function($f3) {
     //echo "Order Form Part I";
 
     // If the form has been posted
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+        // Initialize variables
         $food = "";
         $meal = "";
 
+        // Validate the data
         if (validFood($_POST['food'])) {
             $food = $_POST['food'];
-        } else{
-            $f3->set('errors["food"]', "Invalid Food");
+        }
+        else {
+            $f3->set('errors["food"]', "Invalid food");
         }
 
-        if ((validMeal($_POST['meal'])) and (isset($_POST['meal'])) ) {
+        if (isset($_POST['meal']) and validMeal($_POST['meal'])) {
             $meal = $_POST['meal'];
-        } else{
-            $f3->set('errors["meal"]', "Invalid Meal");
+        }
+        else {
+            $f3->set('errors["meal"]', "Invalid meal");
         }
 
-        $meal = $_POST['meal'];
-
+        // If there are no errors
         if (empty($f3->get('errors'))) {
 
-        // Put the data in the session array
-        $f3->set('SESSION.food', $food);
-        $f3->set('SESSION.meal', $meal);
+            // Put the data in the session array
+            $f3->set('SESSION.food', $food);
+            $f3->set('SESSION.meal', $meal);
 
-        // Redirect to order2 route
-        $f3->reroute('order2');
+            // Redirect to order2 route
+            $f3->reroute('order2');
         }
     }
+
+    // Get data from the model and add to the F3 "hive"
+    $f3->set('meals', getMeals());
 
     // Display a view page
     $view = new Template();
@@ -76,16 +82,17 @@ $f3->route('GET|POST /order1', function ($f3) {
 });
 
 // Define a order form 2 route
-$f3->route('GET|POST /order2', function ($f3) {
+$f3->route('GET|POST /order2', function($f3) {
     //echo "Order Form Part II";
 
     // If the form has been posted
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         // Validate the data
-        if (isset($_POST['conds'])) {
+        if (isset($_POST['conds'])){
             $conds = implode(", ", $_POST['conds']);
-        } else {
+        }
+        else {
             $conds = "None selected";
         }
 
@@ -97,6 +104,9 @@ $f3->route('GET|POST /order2', function ($f3) {
 
     }
 
+    // Add data to the F3 "hive"
+    $f3->set('condiments', getCondiments());
+
     // Display a view page
     $view = new Template();
     echo $view->render('views/order-form-2.html');
@@ -104,7 +114,7 @@ $f3->route('GET|POST /order2', function ($f3) {
 });
 
 // Define an order summary route
-$f3->route('GET /summary', function () {
+$f3->route('GET /summary', function() {
     //echo "Thank you for your order!";
 
     // Display a view page
--- model/validator.php
+++ model/validator.php
@@ -1,12 +1,17 @@
 <?php
+
+/*
+ * 328/diner/model/validate.php
+ * Validate data for the diner app
+ */
+
+// Return true if food is valid
 function validFood($food)
 {
-    if (trim($food) == "") {
+    if (trim($food) == "")
         return false;
-    }
-    if (!ctype_alpha($food)) {
+    if (!ctype_alpha($food))
         return false;
-    }
     return true;
 }
 
